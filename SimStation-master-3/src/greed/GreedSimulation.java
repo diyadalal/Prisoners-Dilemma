103f32e864c97445d5a966e2be60880b527031b1
diyadalal <87991739+diyadalal@users.noreply.github.com>
2025-11-14 22:39:58 -0800

    Delete SimStation-master-3/src/greed directory

--- SimStation-master-3/src/greed/GreedSimulation.java
+++
@@ -1,8 +0,0 @@
-package greed;
-
-public class GreedSimulation {
-    public static void main(String[] args) {
-        GreedPanel panel = new GreedPanel(new GreedFactory());
-        panel.display();
-    }
-}
